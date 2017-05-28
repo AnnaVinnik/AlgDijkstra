43f722864500de21a2091000016a733e48565023
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-28 14:07:43 +0700

    Считает правильные пути

--- main.c
+++ main.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
-#define MAXPATH 3
+#define MAXPATH 5
 
 int main()
 {
-int adjacencyMatrix[MAXPATH][MAXPATH];
+int adjacencyMatrix[MAXPATH][MAXPATH] = {{0, 2, 5, 0, 4}, {2, 0, 3, 0, 0}, {5, 3, 0, 3, 0}, {0, 0, 3, 0, 2}, {4, 0, 0, 2, 0}};
 int shortestPaths[MAXPATH] = {};
 int first, i, j;
     for ( i = 0; i < MAXPATH; i++)
         shortestPaths[i] = 1000;
 
     //Заполнение матрицы
-    printf("Заполнение матрицы");
+   /* printf("Заполнение матрицы");
     for (j = 0; j < MAXPATH; j++){
         for (i = 0; i < MAXPATH; i++){
             scanf("%d", &adjacencyMatrix[j][i]);
         }
-    }
+    }*/
 shortestPaths[0] = 0;
     first = adjacencyMatrix[0][0]; //Обозначаем исходную точку
     for (j = 0; j < MAXPATH; j++){
-printf("!");
-        for (i = 0; i < MAXPATH - 1; i++){
-printf(":)");
+        for (i = 0; i < MAXPATH; i++){
             if (adjacencyMatrix[j][i] > 0){
-printf("=)");
-                if (shortestPaths[i + 1] > shortestPaths[j] + adjacencyMatrix[j][i]){
-                    shortestPaths[i + 1] = shortestPaths[j] + adjacencyMatrix[j][i];
+                if (shortestPaths[i] > shortestPaths[j] + adjacencyMatrix[j][i]){
+                    shortestPaths[i] = shortestPaths[j] + adjacencyMatrix[j][i];
                 }
             }
         }
